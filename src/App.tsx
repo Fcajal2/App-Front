83620e058089848ffeb15f0eecdbfa4c020a40b3
Fcajal <francajal98@gmail.com>
2022-10-27 18:39:03 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+};
 
 function App() {
+  const [products, setProducts] = useState<Product[]>();
+  const [inputs, setInputs] = useState({ name: "", brand: "", price: 0 });
+  useEffect(() => {
+    fetch("https://classy-dusk-90355f.netlify.app/").then((result) => {
+      result.json().then((data: Product[]) => {
+        setProducts(data);
+      });
+    });
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch("https://classy-dusk-90355f.netlify.app/", {
+      method: "POST",
+      body: JSON.stringify({ ...inputs, price: Number(inputs.price) }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((result) => {
+      result.json().then((data) => {
+        console.log(data);
+      });
+    });
+  };
+
+  // const handleDelete = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   fetch("https://classy-dusk-90355f.netlify.app/", {
+  //     method: "DELETE",
+  //   }).then((result) => {
+  //     result.json().then((data) => {
+  //       console.log(data);
+  //     });
+  //   });
+  // };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //@ts-ignore
+    inputs[e.target.name] = e.target.value;
+    setInputs({ ...inputs });
+  };
+
+  console.log("inputs", inputs);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="d-flex m-1">
+        {products?.map((product) => {
+          return (
+            <div className="d-flex">
+              <div key={product.id} className="m-1">
+                <div className="bg-primary p-1">
+                  <p>{product.name}</p>
+                  <p>{product.brand}</p>
+                  <p>{product.price}</p>
+                  {/* <button onClick={handleDelete}>Delete</button> */}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input placeholder={"name"} name={"name"} onChange={handleChange} />
+        <input placeholder={"brand"} name={"brand"} onChange={handleChange} />
+        <input
+          placeholder={"price"}
+          type={"number"}
+          name={"price"}
+          onChange={handleChange}
+        />
+        <button>Crear</button>
+      </form>
     </div>
   );
 }
